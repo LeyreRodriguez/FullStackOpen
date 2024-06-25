985d229d8c285412e866b124506ec891d5efc2e3
LeyreRodriguez <leyre.rodriguez104@alu.ulpgc.es>
2024-06-25 18:29:30 +0100

    Exercise 1.11

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -72,16 +72,18 @@ const Statistics = ({good, neutral, bad, all, average, positive}) => {
 
   return(
     <div>
-        <StatisticLine text="good" value = {good} />
-        <StatisticLine text="neutral" value = {neutral} />
-
-        <StatisticLine text="bad" value = {bad} />
-        <StatisticLine text="all" value = {all} />
-        <StatisticLine text="average" value = {average} />
-        <StatisticLine text="positive" value = {positive} />
-
-
+      <table>
+        <tbody>
+          <StatisticLine text="good" value = {good} />
+          <StatisticLine text="neutral" value = {neutral} />
+          <StatisticLine text="bad" value = {bad} />
+          <StatisticLine text="all" value = {all} />
+          <StatisticLine text="average" value = {average} />
+          <StatisticLine text="positive" value = {positive} />
+        </tbody>
+      </table>
     </div>
+    
   )
 }
 
@@ -89,19 +91,20 @@ const StatisticLine = ({text, value}) =>{
 
   if(text === "positive") {
     return (
-      <div>
-        {text} {value} %
-      </div>
+        <tr>
+          <td>{text}</td>
+          <td>{value} %</td>
+        </tr>
+         
     )
   }
 
   return(
-    <div>
-        <div>
-        {text} {value} 
-      </div>
+        <tr>
+          <td>{text}</td>
+          <td>{value} </td>
+        </tr>
 
-    </div>
   )
 }
 
