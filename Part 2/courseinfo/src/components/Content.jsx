ee6db019964564a9a57c60506a0a50e8c75049ef
LeyreRodriguez <leyre.rodriguez104@alu.ulpgc.es>
2024-06-26 15:47:19 +0100

    Exercise 2.2

--- Part 2/courseinfo/src/components/Content.jsx
+++ Part 2/courseinfo/src/components/Content.jsx
@@ -1,12 +1,22 @@
 import Part from "./Part"
+import Total from "./Total"
+
 
 const Content = ({parts}) => {
+
+    let totalExercises = 0;
+    parts.forEach(part => {
+      totalExercises += part.exercises;
+    });
+
     return (
       <div>
 
          {parts.map((part, index) => (
           <Part key={index} name={part.name} exercises={part.exercises} />
         ))} 
+
+        <Total amount = {totalExercises} />
       </div>
     )
   }
---
+++ Part 2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,10 @@
+const Total = ({ amount }) => (
+    
+    <div>
+      <h3> total of {amount} exercises </h3>
+    </div>
+  )
+
+  export default Total
+
+
