a114784a0df085b138388ad542c416a60388955a
LeyreRodriguez <leyre.rodriguez104@alu.ulpgc.es>
2024-07-03 17:08:25 +0100

    Exercise 3.20

--- Part 3/phoneBookBackend/models/person.js
+++ Part 3/phoneBookBackend/models/person.js
@@ -25,8 +25,19 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^(\d{2,3})-\d{4,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
+
+
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
