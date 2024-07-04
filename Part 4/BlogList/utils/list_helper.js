74fc1ee2140d9c31918ab3943d1317187923c321
LeyreRodriguez <leyre.rodriguez104@alu.ulpgc.es>
2024-07-04 13:27:45 +0100

    Exercise 4.4

--- Part 4/BlogList/utils/list_helper.js
+++ Part 4/BlogList/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1;
   }
 
+
+const totalLikes = (blogs) => {
+    let likes = 0;
+
+    for ( i = 0; i < blogs.length; i++ ) {
+        likes += blogs[i].likes
+    }
+
+    return likes
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
