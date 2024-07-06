5c7ca11c85e1666fc13401a02c2d670a687f3667
LeyreRodriguez <leyre.rodriguez104@alu.ulpgc.es>
2024-07-06 18:34:48 +0100

    Exercise 4.19

--- Part 4/BlogList/controllers/blogs.js
+++ Part 4/BlogList/controllers/blogs.js
@@ -3,9 +3,17 @@ const express = require('express')
 require('express-async-errors')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 
-
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 
   blogsRouter.get('/', async (request, response) => {
@@ -21,7 +29,12 @@ const User = require('../models/user')
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
 
     const blog = new Blog({
       title: body.title,
